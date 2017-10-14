962abe74072605c644ab2050c93ef0e937e751df
ColemanJohnston <colemanmjohnston@student.hartnell.edu>
2017-10-13 20:22:53 -0700

    First commit

---
+++ app/src/main/java/com/example/cst438_hungry_hungry_programmers/swipe_n_dine/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cst438_hungry_hungry_programmers.swipe_n_dine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
