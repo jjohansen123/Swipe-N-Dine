aabd9a415abff32ecda148bfe42f5f6aee00a0d2
David Jia <davidjia0809@gmail.com>
2017-11-30 02:37:43 -0800

    Prototype Login Page

--- app/src/main/java/com/example/cst438_hungry_hungry_programmers/swipe_n_dine/MainActivity.java
+++ app/src/main/java/com/example/cst438_hungry_hungry_programmers/swipe_n_dine/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.cst438_hungry_hungry_programmers.swipe_n_dine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText usernameInput;
+    EditText passwordInput;
+    Button submitLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameInput = (EditText) findViewById(R.id.usernameInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+        submitLogin = (Button) findViewById(R.id.loginSubmit);
     }
 }
