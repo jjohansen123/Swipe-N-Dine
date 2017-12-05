1e95fd873ce7a4ab06ddf68484dc746c1b127891
jjohansen123 <31460346+jjohansen123@users.noreply.github.com>
2017-12-04 22:48:39 -0800

    first test of selectionPage

--- app/src/main/java/com/example/cst438_hungry_hungry_programmers/swipe_n_dine/selectionPage.java
+++ app/src/main/java/com/example/cst438_hungry_hungry_programmers/swipe_n_dine/selectionPage.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
 public class selectionPage extends AppCompatActivity {
     TextView mRestaurantTitle, mRateCat;
     ImageView mMainImage;
