48a3215c6c24a7dc68086010deae31b52d81d3e7
David Jia <davidjia0809@gmail.com>
2017-12-06 22:54:34 -0800

    Added Yelp Funcitonality

--- app/src/main/java/com/example/cst438_hungry_hungry_programmers/swipe_n_dine/SelectionPage.java
+++ app/src/main/java/com/example/cst438_hungry_hungry_programmers/swipe_n_dine/SelectionPage.java
@@ -1,8 +1,17 @@
 package com.example.cst438_hungry_hungry_programmers.swipe_n_dine;
 
-import android.os.AsyncTask;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,22 +21,36 @@ import com.squareup.picasso.Picasso;
 import com.yelp.fusion.client.connection.YelpFusionApi;
 import com.yelp.fusion.client.connection.YelpFusionApiFactory;
 import com.yelp.fusion.client.models.Business;
+import com.yelp.fusion.client.models.SearchResponse;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class SelectionPage extends AppCompatActivity {
-    TextView mRestaurantTitle, mRateCat;
-    ImageView mMainImage;
 
-    private final String CLIENT_ID_KEY = getString(R.string.clientId);
-    private final String CLIENT_SECRET_KEY = getString(R.string.clientSecret);
+    TextView mRestaurantTitle;
+    ImageView mMainImage;
 
     YelpFusionApiFactory yelpFusionApiFactory;
     YelpFusionApi yelpFusionApi;
+
+    ArrayList<Business> businesses;
+    ArrayList<String> businessNames;
+    ArrayList<String> businessImages;
+    ArrayList<String> businessUrls;
+    String currentUrl;
+    boolean isAtStart = true;
+    boolean updateFirst = true;
+    int numOfSearchResults = 0;
+    int currentBusinessIndex = 0;
+
+    LocationManager mLocationManager;
+    double latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,31 +60,42 @@ public class SelectionPage extends AppCompatActivity {
         thread.start();
 
         mRestaurantTitle = (TextView) findViewById(R.id.titleLabel);
-        mRateCat = (TextView) findViewById(R.id.rateCatLabel);
         mMainImage = (ImageView) findViewById(R.id.mainImage);
 
-        Picasso
-                .with(this)
-                .load("http://i.imgur.com/DvpvklR.png")
-                .into(mMainImage);
+        businessNames = new ArrayList<String>();
+        businessImages = new ArrayList<String>();
+        businessUrls = new ArrayList<String>();
+
         mMainImage.setOnTouchListener(new OnSwipeTouchListener(this){
             public void onSwipeTop() {
-                Toast.makeText(SelectionPage.this, "top", Toast.LENGTH_SHORT).show();
+
             }
             public void onSwipeRight() {
-                Toast.makeText(SelectionPage.this, "right", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(SelectionPage.this, "right", Toast.LENGTH_SHORT).show();
             }
             public void onSwipeLeft() {
-                Toast.makeText(SelectionPage.this, "left", Toast.LENGTH_SHORT).show();
-                Call<Business> call = yelpFusionApi.getBusiness("gary-danko-san-francisco");
-                call.enqueue(callback);
+                if (isAtStart) {
+                    Toast.makeText(SelectionPage.this, "Loading . . .", Toast.LENGTH_SHORT).show();
+                    Map<String, String> params = new HashMap<>();
+
+                    params.put("term", "food");
+                    params.put("location", "Seaside");
+
+                    Call<SearchResponse> call = yelpFusionApi.getBusinessSearch(params);
+                    call.enqueue(callback);
+                } else {
+                    mRestaurantTitle.setText(businessNames.get(currentBusinessIndex));
+                    Picasso.with(getApplicationContext())
+                        .load(businessImages.get(currentBusinessIndex))
+                        .into(mMainImage);
+                    currentUrl = businessUrls.get(currentBusinessIndex);
+                    currentBusinessIndex++;
+                }
             }
             public void onSwipeBottom() {
-                Toast.makeText(SelectionPage.this, "bottom", Toast.LENGTH_SHORT).show();
+
             }
         });
-
-//        new FetchPictures().execute();
     }
 
     Thread thread = new Thread(new Runnable(){
@@ -69,44 +103,52 @@ public class SelectionPage extends AppCompatActivity {
         public void run() {
             try {
                 yelpFusionApiFactory = new YelpFusionApiFactory();
-                yelpFusionApi = yelpFusionApiFactory.createAPI(CLIENT_ID_KEY, CLIENT_SECRET_KEY);
+                yelpFusionApi = yelpFusionApiFactory.createAPI(getString(R.string.clientId), getString(R.string.clientSecret));
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     });
 
-    Callback<Business> callback = new Callback<Business>() {
+    Callback<SearchResponse> callback = new Callback<SearchResponse>() {
         @Override
-        public void onResponse(Call<Business> call, Response<Business> response) {
-            Business business = response.body();
-            ArrayList<String> d = business.getLocation().getDisplayAddress();
-            StringBuilder sbStr = new StringBuilder();
-            for (int i = 0, il = d.size(); i < il; i++) {
-                if (i > 0)
-                    sbStr.append(',');
-                sbStr.append(d.get(i));
+        public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
+            SearchResponse searchResponse = response.body();
+            businesses = searchResponse.getBusinesses();
+            numOfSearchResults = searchResponse.getTotal();
+            for (int i = 0; i < 20; i++) {
+                businessNames.add(businesses.get(i).getName());
+                businessImages.add(businesses.get(i).getImageUrl());
+                businessUrls.add(businesses.get(i).getUrl());
             }
-            mRestaurantTitle.setText(sbStr.toString());
+            mRestaurantTitle.setText(businessNames.get(currentBusinessIndex));
+            Picasso.with(getApplicationContext())
+                    .load(businessImages.get(currentBusinessIndex))
+                    .into(mMainImage);
+            currentUrl = businessUrls.get(currentBusinessIndex);
+            currentBusinessIndex++;
 
-            // Update UI text with the Business object.
+            isAtStart = false;
         }
 
         @Override
-        public void onFailure(Call<Business> call, Throwable t) {
-            mRestaurantTitle.setText(t.getMessage());
+        public void onFailure(Call<SearchResponse> call, Throwable t) {
+            Log.v("Error Message", t.getMessage());
             // HTTP error happened, do something to handle it.
         }
     };
-    // Doing the API in the background
-    // String initilise - string updates - string ignore
-    class FetchPictures extends AsyncTask<String, String, String> {
 
-        @Override
-        protected String doInBackground(String ... params) {
+    public void openBrowser(View view){
+        //Get url from tag
+        if (!isAtStart) {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.addCategory(Intent.CATEGORY_BROWSABLE);
 
-            return null;
+            //pass the url to intent data
+            intent.setData(Uri.parse(currentUrl));
+
+            startActivity(intent);
         }
     }
-
 }
